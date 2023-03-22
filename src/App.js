4341dd541e65a79dc01607558c7332e3c8b949ef
Shivamp1234 <patilshivam156@gmail.com>
2023-03-22 22:05:16 +0530

    Uploaded React handson 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import FunctionalComponent from "./Components/FunctionalComponent";
+import ClassComponent from "./Components/ClassComponent";
+import { useState } from "react";
+import "./style.css"
 
 function App() {
+  const[count,setCount]=useState(false);
+  const[data,setData]=useState(false);
+  const handleFunction=()=>{
+    setCount(!count);
+  }
+  const handleClass=()=>{
+    setData(!data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="main">
+    <h1 className="heading">Styling using Functional and Class Component</h1>
+    <button onClick={handleFunction} className="btn1">To see styling in functional component</button>
+    <button onClick={handleClass} className="btn2">To see styling in class component</button>
+    <div className="components">
+    {count?<FunctionalComponent/>:null}
+    {data?<ClassComponent/>:null}
     </div>
+    </div>
+    </>
   );
 }
 
